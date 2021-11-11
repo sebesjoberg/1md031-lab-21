25bf022b1dae71fcf9784403179106fee65e30b1
Sebastian <ss69109@gmail.com>
2021-11-11 17:04:51 +0100

    added my burgers in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div>
     Burgers
     <Burger v-for="burger in burgers"
-            v-bind:burger="burger" 
+            v-bind:burger="burger"
             v-bind:key="burger.name"/>
   </div>
   <div id="map" v-on:click="addOrder">
@@ -13,8 +13,25 @@
 <script>
 import Burger from '../components/Burger.vue'
 import io from 'socket.io-client'
-
 const socket = io();
+
+const Items = [
+  new MenuItem('The Bro Burger','https://cdn.shopify.com/s/files/1/0372/9054/1115/products/hampshirehamburgarekopiera_1200x1200.jpg?v=1627649844'
+  , 1000, 'true', 'true'),new MenuItem('Fullkornsburgaren','http://skippanudlarna.files.wordpress.com/2012/05/egna-hamburgarestorbild.jpg'
+  , 500, true, false), new MenuItem('Klassikern','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa38lJgnL3-bs2NQhc2pOdPsjyQSKoP6CiGA&usqp=CAU',
+  1500,true,true)
+
+]
+console.log(Items);
+
+
+
+function MenuItem(name, URL, KCal, Gluten, Lactose){
+  this.name=name;
+  this.URL=URL;
+  this.Gluten=Boolean(Gluten);
+  this.Lactose=Boolean(Lactose);
+}
 
 export default {
   name: 'Home',
@@ -23,10 +40,7 @@ export default {
   },
   data: function () {
     return {
-      burgers: [ {name: "small burger", kCal: 250},
-                 {name: "standard burger", kCal: 450},
-                 {name: "large burger", kCal: 850}
-               ]
+      burgers: Items
     }
   },
   methods: {
