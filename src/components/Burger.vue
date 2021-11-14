c3e1c58cbf35d3c68baccb3cd5abd792165a02f8
Sebastian <ss69109@gmail.com>
2021-11-14 22:41:55 +0100

    changed some

--- src/components/Burger.vue
+++ src/components/Burger.vue
@@ -2,17 +2,21 @@
 
   <div>
   <section class="burgers">
+
     <div v-bind:class="burger.number"> <h5 class="burgernames">{{ burger.name }}</h5>
       <img v-bind:src="burger.URL"
       v-bind:title="burger.name"  width="400" height="300">
       <ul>
-        <section class="innehåll"><li>Innehåller gluten</li>
+
+        <section class="innehåll" v-if="burger.Gluten"><li>Innehåller gluten</li>
         </section>
-        <section class="innehåll"><li>Innehåller laktos</li>
+
+        <section class="innehåll" v-if="burger.Lactose"><li>Innehåller laktos</li>
         </section>
-        <section class="innehåll"> <li> OBS! stark</li>
-        </section>
+
+        
         </ul>
+
       </div>
 </section>
     </div>
@@ -20,14 +24,15 @@
 </template>
 
 <script>
-
-
 export default {
   name: 'Burger',
   props: {
     burger: Object
-  }
-}
+  }}
+
+
+
+
 
 </script>
 
